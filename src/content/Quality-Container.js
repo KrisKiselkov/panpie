775e6dac9685c5cb309f4639b76917810ff6aefe
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-07-08 16:54:22 +0300

    Quality Container Base Look

--- src/content/Quality-Container.js
+++ src/content/Quality-Container.js
@@ -3,7 +3,15 @@
 export function QualityContainer() {
     return(
         <section id="quality-container">
-            
+            <div id="quality-text">
+                <h1 id="quality-h1">We Have <span>Excellent</span><br></br>
+                Of <span>Quality</span> Pizza</h1>
+                <button id="quality-btn">See Full Menu</button>
+            </div>
+
+            <div id="quality-image-container">
+                <img id="quality-image" src={require("../images/quality-pizza.png")}></img>
+            </div>
         </section>
     );
 }
