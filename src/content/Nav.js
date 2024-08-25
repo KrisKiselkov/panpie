9e633bb937815083bbfe454824abff151a70de85
KrisKiselkov <cristian.kiselkov@gmail.com>
2024-08-26 00:28:36 +0300

    Menu Header And Breadcrumbs

--- src/content/Nav.js
+++ src/content/Nav.js
@@ -1,6 +1,24 @@
 import { NavLink } from "react-router-dom"
 
 export function Nav() {
+    window.addEventListener('scroll', () => {
+        const header = document.querySelector('header');
+        const nav = document.querySelector('nav');
+        const scrollHeight = window.scrollY;
+        
+        header.classList.toggle('sticky', scrollHeight > 0);
+        if (scrollHeight) {
+            header.style.height='100px';
+            header.style.boxShadow='0px 3px 5px #18181818';   
+            nav.style.marginTop='-4px';
+        } else {
+            header.style.height='110px';
+            header.style.boxShadow='0px 1px #18181818';
+            nav.style.marginTop='0px';
+        }
+    })
+
+
     return (
         <>
                 <header>
--- src/content/menu-content/Menu-Page.js
+++ src/content/menu-content/Menu-Page.js
@@ -5,7 +5,36 @@ export function Menu() {
         <>
             <Nav />
             <section id="menu-section">
+                <section id="menu-header">
+                    <h1 id="menu-h1">Menu</h1>
 
+                    <ol className='nav-bc'>
+                        <li className='li-bc'><a href='/'>Home</a></li>
+                        <li className='li-bc li-bc-active'>Menu</li>
+                    </ol>
+                </section>
+
+                <section id="menu-main">
+                    <div id="menu-grid">
+                        <div id="menu-filter-col">
+                            <div id='menu-filter-content'>
+                                <div id='menu-search-div'>
+                                    <input type='search' id='menu-search' placeholder='Search products...'></input>
+                                </div>
+
+                                <div id='menu-categories-div'>
+                                    <div id='menu-filter-categories'>
+                                        <h2 id='categories-h2'>Categories</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="menu-products-col">
+
+                        </div>
+                    </div>
+                </section>
             </section>
         </>
     );
